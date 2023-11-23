904ea534ec48833f9b77ecb4e0100c7f8ca9a68f
EnricoDG97 <enrico.delgaudio97@gmail.com>
2023-11-23 21:42:18 +0100

    feat: added scaffolding footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -10,14 +10,53 @@
 </script>
 
 <template>
- <p>
-   footer
- </p>
+    <section class="top-footer flex">
+        <nav class="left-side">
+            <div class="col-1">
+                <h2>dc comics</h2>
+                <ul>
+                    <li><a href="">prova</a></li>
+                </ul>
+                <h2>shop</h2>
+                <ul>
+                    <li><a href="">prova</a></li>
+                </ul>
+            </div>
+            <div class="col-2">
+                <h2>dc</h2>
+                <ul>
+                    <li><a href="">prova</a></li>
+                </ul>
+            </div>
+            <div class="col-3">
+                <h2>sites</h2>
+                <ul>
+                    <li><a href="">prova</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class="right-side logo-bg">
+            logo bg
+        </div>
+    </section>
+    <section class="bottom-footer">
+        <div class="left-side">
+            <button>sign-up</button>
+        </div>
+        <div class="right-side">
+            <h2>follow us:</h2>
+            <div class="icons">
+                <span>icon</span>
+                <span>icon</span>
+                <span>icon</span>
+                <span>icon</span>
+            </div>
+        </div>
+    </section>
 </template>
 
 <style lang="scss" scoped>
 @use "../style/partials/mixins" as *;
 @use "../style/partials/variables" as *;
-
 
 </style>
