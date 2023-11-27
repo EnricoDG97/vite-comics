b746207309d100461acd9487c6db6a35c2691bd6
EnricoDG97 <enrico.delgaudio97@gmail.com>
2023-11-27 16:27:43 +0100

    feat: added card component and props

---
+++ src/components/AppSerieCard.vue
@@ -0,0 +1,43 @@
+<script>
+    export default {
+        props: {
+            image: String,
+            title: String,
+        },
+        data(){
+            return {}
+        },
+        methods: {
+            getImageUrl(imgName) {
+                return new URL(imgName, import.meta.url).href;
+            },
+        },
+    };
+</script>
+
+<template>
+    <div class="card">
+        <img :src="getImageUrl(image)" :alt="title" />
+        <h2>
+            {{ title }}
+        </h2>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use "../style/partials/mixins" as *;
+@use "../style/partials/variables" as *;
+
+.card {
+    img {
+        width: 100%;
+        height: 200px;
+    }
+    h2 {
+        font-size: small;
+        text-transform: uppercase;
+        color: white;
+    }
+}
+
+</style>
